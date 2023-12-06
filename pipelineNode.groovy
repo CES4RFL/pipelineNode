180c66b714fa62977193969a91fb0fc3a74e201a
CES4RFL <cesar.afl@outlook.com>
2023-12-05 22:01:53 -0600

    first commit

---
+++ pipelineNode.groovy
@@ -0,0 +1,75 @@
+def IMAGE_VERSION = 'latest'
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main',
+                credentialsId: 'GITHUB',
+                url: 'https://github.com/CES4RFL/ExpressApp.git'
+            }
+        }
+        
+        
+        stage('Test') {
+            steps {
+                sh 'npm install'
+                sh 'NODE_OPTIONS=--experimental-vm-modules npx jest'
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                
+                script{
+                    IMAGE_VERSION = sh(script: "node -p \"require('./package.json').version\"", returnStdout: true).trim()
+                    echo "${IMAGE_VERSION}"
+                }
+                
+                sh """  docker build -f Dockerfile -t public.ecr.aws/f0o1f7t2/appsnao:${IMAGE_VERSION} .
+                        docker images
+                        docker system prune -a
+                    """
+            }
+        }
+        
+        stage('Setup credentials') {
+            steps {
+
+                withCredentials([
+                   string(credentialsId: 'aws_access_key_id', variable: 'aws_access_key_id'),
+                   string(credentialsId: 'aws_secret_access_key', variable: 'aws_secret_access_key')
+                ]){
+                    sh """
+                        #!/bin/bash
+                        aws configure set aws_access_key_id $aws_access_key_id
+                        aws configure set aws_secret_access_key $aws_secret_access_key
+                        aws configure set default.region us-west-2 --output json --profile dev
+                        aws configure list
+                        aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/f0o1f7t2
+                    """
+                }
+            }
+        } 
+        
+        stage('Docker push') {
+            steps {
+                sh """
+         	      docker push public.ecr.aws/f0o1f7t2/appsnao:${IMAGE_VERSION}
+                """
+            }
+        }
+    } 
+    post { 
+        always { 
+            deleteDir()
+            sh 'rm -r ~/.aws'
+            script{
+                def exitCode = sh(script:"docker rmi -f \$(docker images -aq)", returnStatus: true)
+                if (exitCode != 0) {
+                    echo "exit 0"
+                }
+            }
+        }
+    }
+}
